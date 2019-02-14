686e1c7eaca35e27eaeab1578a337edc358e5fa8
Rohith Samuel <rohith.sml94@gmail.com>
2019-02-14 16:37:03 +0530

    RED: Test fot initiating pre-defined live cells

--- test_Game_of_life.py
+++ test_Game_of_life.py
@@ -4,6 +4,12 @@ import Game_of_life
 
 #1.Creating a nxn matrix
 
-def test_4x4_matrix():
-    expected_value=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    assert Game_of_life.matrix(4) == expected_value
+def test_3x3_matrix():
+    expected_value=[[0,0,0],[0,0,0],[0,0,0]]
+    assert Game_of_life.matrix(3) == expected_value
+
+def test_initializing_predefined_live_cells():
+    matrix=[[0,1,0],[0,0,0],[0,0,0]]
+    expected_value= [[0,1,0],[0,1,0],[0,1,0]]
+    assert Game_of_life.initial(matrix)== matrix
+    
